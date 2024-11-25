ef0fa7f92cfa02b1c342c07ad7b0f8b36c1e2f37
Elena Fuentes Bongenaar <e.fuentesbongenaar@fastmail.com>
2024-11-25 11:52:20 -0600

    Fix for printing Rust test values

--- scripts/prepare_rust_dataset.py
+++ scripts/prepare_rust_dataset.py
@@ -10,31 +10,31 @@ if __name__ == "__main__":
     dataset = pd.read_csv("./datasets/train_data.csv")
 
     # Print data for Rust
-    print("let inputs = vec![")
+    print("let inputs: [[f64; M]; N] = [")
     for i in range(dataset.shape[0]):
-        print("\tvec![")
+        print("\t[")
         for j in range(4):
             print(f"\t\t{dataset.iloc[i, j]:.6},")  # f64 format
         print("\t],")
     print("];\n")
 
-    print("================== LABELS ===========================")
+    print("//================== LABELS ===========================")
 
-    print("let labels = vec![")
+    print("let labels_class0 = [")
     for i in range(dataset.shape[0]):
         print(f"\t{dataset.iloc[i, -1]:.1f},")  # Force 0.0 or 1.0 format
     print("];")
 
-    print("================== LABELS ===========================")
+    print("//================== LABELS ===========================")
 
-    print("let labels = vec![")
+    print("let labels_class1 = [")
     for i in range(dataset.shape[0]):
         print(f"\t{dataset.iloc[i, -2]:.1f},")  # Force 0.0 or 1.0 format
     print("];")
 
-    print("================== LABELS ===========================")
+    print("//================== LABELS ===========================")
 
-    print("let labels = vec![")
+    print("let labels_class2 = [")
     for i in range(dataset.shape[0]):
         print(f"\t{dataset.iloc[i, -3]:.1f},")  # Force 0.0 or 1.0 format
     print("];")
